fbe112906db3fc70606d865ebeebe7407f26f191
sinwar <premsinwar4@gmail.com>
2019-01-13 18:09:35 +0530

    Add contact model

--- eeshala/eshalahome/models.py
+++ eeshala/eshalahome/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
 
@@ -9,3 +9,18 @@ class pdfs(models.Model):
     """
     title = models.CharField(default="pdf", max_length = 200)
     file = models.FileField(upload_to='eshalahome/static/pdf')
+
+
+class ContactMessage(models.Model):
+	"""
+	model to save contact form data
+	"""
+	sendername = models.CharField(default="annonymous",
+	 							  max_length=200,
+	 							  verbose_name=_('Sender Name'))
+	senderemail = models.EmailField(max_length=400,
+									blank=True,
+									null=True,
+									verbose_name=_('Sender Email'))
+	message = models.TextField(verbose_name=_('Query'),
+								help_text=_('Type your query, Please be explicit'))
